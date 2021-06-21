be130b72a39af3fc784849253183dba266ea73b7
곰다방 <gomdabang.js@gmail.com>
2021-06-21 17:50:40 +0900

    Update 294300.js

--- 294300.js
+++ 294300.js
@@ -1,11 +1,14 @@
-const selectedSeat = '이건 어떤건지 몰라서 그냥 막 이렇게 했어요';
 const inputList = document.querySelectorAll('input[name="seatId"]'); 
 
-for ( let i = 0; i < inputList.length; i++ ) {
-    let el = inputList[i]; 
+function checkSeat(selectedSeat) {
+    for ( let i = 0; i < inputList.length; i++ ) {
+        let el = inputList[i]; 
 
-    if ( el.value === selectedSeat ) {
-        // 같은 값 
-        el.disabled = false; 
+        if ( el.value === selectedSeat ) {
+            // 같은 값 
+            el.disabled = false; 
+        }
     }
 }
+
+checkSeat('seat00'); // 한번만 동작 
