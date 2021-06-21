36163881f77402d48cedee9bea7845f101a74eac
곰다방 <gomdabang.js@gmail.com>
2021-06-21 17:58:39 +0900

    Update 294300.js

--- 294300.js
+++ 294300.js
@@ -1,9 +1,11 @@
 const inputList = document.querySelectorAll('input[name="seatId"]'); 
 
+// 예제1
+// 한번만 확인 
+
 function checkSeat(selectedSeat) {
     for ( let i = 0; i < inputList.length; i++ ) {
         let el = inputList[i]; 
-
         if ( el.value === selectedSeat ) {
             // 같은 값 
             el.disabled = false; 
@@ -11,4 +13,23 @@ function checkSeat(selectedSeat) {
     }
 }
 
-checkSeat('seat00'); // 한번만 동작 
+checkSeat('seat00'); 
+
+
+// 에제2
+// 한번만 확인 
+let seatIdList = []; 
+for ( let i = 0; i < inputList.length; i++ ) {
+    let el = inputList[i];
+    seatIdList.push( el.value ); 
+}
+
+function checkSeat2(selectedSeat) {
+    let index = seatIdList.indexOf(selectedSeat);
+    if ( index > -1 ) {
+        // 같은 값 
+        inputList[index].disabled = false; 
+    }
+}
+
+checkSeat2('seat00');
